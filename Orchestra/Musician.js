838860a140e618047aa57161151f2567e7eaaa5e
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-05-06 08:58:02 +0200

    [WIP] musician.js, added udpProtocol.js

--- Orchestra/Musician.js
+++ Orchestra/Musician.js
@@ -2,4 +2,15 @@
  * RES - Laboratoire Orchestra
  * Student: Musician.js
  * Date: 02.05.2019
+ * 
+ * This code specify a musician.
+ * 
+ * Based on thermometer.js by Olivier Liechti.
  */
+
+ var protocol = require('./udpProtocol');
+
+ // Standard node.js module to work with udp
+ var datagram = require('dgram');
+
+ var socket = datagram.createSocket('udp6'); // udp4 or udp6 ?
---
+++ Orchestra/udpProtocol.js
@@ -0,0 +1,7 @@
+/**
+ * Specifications of the default adress and port for the udp protocol.
+ * Based on sensor-protocol.js by Olivier Liechti
+ */
+
+ exports.PROTOCOL_PORTS = 2205;
+ exports.MULTICAST_ADDRESS = "239.255.22.10";
