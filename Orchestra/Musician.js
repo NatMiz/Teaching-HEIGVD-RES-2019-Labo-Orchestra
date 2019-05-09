cc994a2510a1bf699d6c71c9532fdf11fdef2a6f
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-05-09 23:09:07 +0200

    Musician almost done

--- Orchestra/Musician.js
+++ Orchestra/Musician.js
@@ -1,16 +1,58 @@
-/**
- * RES - Laboratoire Orchestra
- * Student: Musician.js
- * Date: 02.05.2019
- * 
- * This code specify a musician.
- * 
- * Based on thermometer.js by Olivier Liechti.
- */
+/*
+  RES - Laboratoire Orchestra
+  Student: Musician.js
+  Date: 02.05.2019
+  This code specify a musician.
 
- var protocol = require('./udpProtocol');
+  Based on thermometer.js by Olivier Liechti.
+*/
 
- // Standard node.js module to work with udp
- var datagram = require('dgram');
+// Standard node.js module to work with udp
+var datagram = require('dgram');
 
- var socket = datagram.createSocket('udp6'); // udp4 or udp6 ?
+const uuidv4 = require('uuid/v4');
+
+var protocol = require('./udpProtocol');
+
+// udp4 or udp6 ?
+var socket = datagram.createSocket('udp4'); 
+
+function Musician(instrument, timeStamp) {
+  this.uuid = uuidv4();
+
+  this.timeCreation = timeStamp;
+
+  this.instrument = instrument;
+
+  this.instrumentSound = function sound() {
+    switch (this.instrument) {
+      case 'piano':
+        return 'ti-ta-ti';
+      case 'trumpet':
+        return 'pouet';
+      case 'flute':
+        return 'trulu';
+      default:
+        return '';
+    }
+  };
+
+  Musician.prototype.update = function () {
+
+  var playSound = {
+    uuid: this.uuid,
+    instrument: this.instrument,
+    activeSince: this.timeCreation
+  };
+
+  var payload = JSON.stringify(playSound);
+
+  message = new Buffer(payload);
+
+  socket.send(message, 0, message.length, protocol.PROTOCOL_PORTS, protocol.MULTICAST_ADDRESS, function(err, bytes){
+    console.log("Sending payload: " + payload + " via port " + s.address().port); // DEbug
+  });
+}
+
+  setInterval(this.update.bind(this), 1000);
+}
