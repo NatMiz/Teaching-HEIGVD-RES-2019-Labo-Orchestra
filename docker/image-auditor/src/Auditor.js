00cebfd85cbe6fdbf1db0920d03758198cd8aba6
NatMiz <nathanael.mizutani@heig-vd.ch>
2019-05-11 17:12:48 +0200

    [WIP] Implementing Auditor.js

--- docker/image-auditor/src/Auditor.js
+++ docker/image-auditor/src/Auditor.js
@@ -3,3 +3,23 @@
  * Student: Musician.js
  * Date: 02.05.2019
  */
+
+ const protocol = require('./udpProtocol');
+
+ const datagram = require('dgram');
+
+ const socket = datagram.createSocket('udp4');
+
+ const moment = require('moment');
+
+ class Auditor {
+     constructor() {
+        this.musicians = new Map();
+     }
+ }
+
+ socket.bind(protocol.PROTOCOL_PORTS,socket.addMembership(protocol.MULTICAST_ADDRESS));
+
+ socket.on('message', function(msg, source){
+    
+ });
---
+++ docker/image-auditor/src/udpProtocol.js
@@ -0,0 +1,10 @@
+/**
+ * Specifications of the default adress and port for the udp protocol.
+ * Based on sensor-protocol.js by Olivier Liechti
+ */
+
+exports.PROTOCOL_PORTS = 2205;
+/* address to use: 239.255.22.10
+   adress for test with netcat: 127.0.0.1
+*/
+exports.MULTICAST_ADDRESS = '239.255.22.10';
